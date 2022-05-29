2116578b23c4c2b9294c07304b4d2a85a6eda1c9
tati-18 <59493650+tati-18@users.noreply.github.com>
2022-05-29 14:50:49 -0500

    corrigiendo experimento 2 (#1)

--- survey/__init__.py
+++ survey/__init__.py
@@ -1,5 +1,6 @@
 from otree.api import *
 
+doc = "DESCRIPTION"
 
 class C(BaseConstants):
     NAME_IN_URL = 'survey'
@@ -16,44 +17,20 @@ class Group(BaseGroup):
 
 
 class Player(BasePlayer):
-    age = models.IntegerField(label='What is your age?', min=13, max=125)
-    gender = models.StringField(
-        choices=[['Male', 'Male'], ['Female', 'Female']],
-        label='What is your gender?',
-        widget=widgets.RadioSelect,
-    )
-    crt_bat = models.IntegerField(
-        label='''
-        A bat and a ball cost 22 dollars in total.
-        The bat costs 20 dollars more than the ball.
-        How many dollars does the ball cost?'''
-    )
-    crt_widget = models.IntegerField(
-        label='''
-        If it takes 5 machines 5 minutes to make 5 widgets,
-        how many minutes would it take 100 machines to make 100 widgets?
-        '''
-    )
-    crt_lake = models.IntegerField(
-        label='''
-        In a lake, there is a patch of lily pads.
-        Every day, the patch doubles in size.
-        If it takes 48 days for the patch to cover the entire lake,
-        how many days would it take for the patch to cover half of the lake?
-        '''
-    )
+    name = models.StringField(label='What is your name?')
+    age = models.IntegerField(label='What is your age?')
 
 
 # FUNCTIONS
 # PAGES
-class Demographics(Page):
+class Survey(Page):
     form_model = 'player'
-    form_fields = ['age', 'gender']
+    form_fields = ['name', 'age']
 
 
-class CognitiveReflectionTest(Page):
+class Results(Page):
     form_model = 'player'
-    form_fields = ['crt_bat', 'crt_widget', 'crt_lake']
+   
 
 
-page_sequence = [Demographics, CognitiveReflectionTest]
+page_sequence = [Survey, Results]
